d15a25a82da37a4b941032d43b88906e23bfc930
scarviz@gmail.com <scarviz@gmail.com@36ffe54a-474d-a20e-a963-f37414e2a6ad>
2011-07-19 11:07:32 +0000

    追加：シナリオリストのActivityとXML追加。 変更：マニフェストにシナリオリストActivityを定義

---
+++ trunk/Selector/src/com/scarviz/selector/ScenarioList.java
@@ -0,0 +1,48 @@
+package com.scarviz.selector;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ScenarioList extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.scenariolist);
+        
+        // ArrayAdapterを生成する
+        ArrayAdapter<String> adapter 
+        = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        // アイテムを追加
+        adapter.add("タイトル１");
+        adapter.add("タイトル２");
+        adapter.add("タイトル３");
+        adapter.add("タイトル４");
+        adapter.add("タイトル５");
+        // リストに設定する
+        getListView().setAdapter(adapter);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		// 選択した行を呼び出し元に返して終了する
+		Intent intent = new Intent();
+		intent.putExtra("TITLE_NO", position);  
+		setResult(RESULT_OK, intent);  
+		finish();  
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
