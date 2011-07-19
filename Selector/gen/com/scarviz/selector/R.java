6eb49c2813036cfc368f79092d80d5b4116c17c8
myu_mn <myu_mn@hotmail.com>
2011-07-19 12:30:17 +0000

    追加：設定画面、シークバー(BGM/SE)、設定用文言 変更：初期化ボタン追加

--- Selector/gen/com/scarviz/selector/R.java
+++ Selector/gen/com/scarviz/selector/R.java
@@ -11,20 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040000;
-        public static final int green=0x7f040005;
-        public static final int orange=0x7f040004;
-        public static final int pink=0x7f040002;
-        public static final int red=0x7f040003;
-        public static final int white=0x7f040001;
+        public static final int black=0x7f050000;
+        public static final int green=0x7f050005;
+        public static final int orange=0x7f050004;
+        public static final int pink=0x7f050002;
+        public static final int red=0x7f050003;
+        public static final int white=0x7f050001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int TextView01=0x7f070002;
-        public static final int btnDecision=0x7f070001;
-        public static final int txtName=0x7f070000;
+        public static final int TextView01=0x7f080002;
+        public static final int btnDecision=0x7f080001;
+        public static final int txtName=0x7f080000;
     }
     public static final class layout {
         public static final int inputinfo=0x7f030000;
@@ -32,18 +32,31 @@ public final class R {
         public static final int title=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f060000;
         /**  ボタン 
          */
-        public static final int decision=0x7f050001;
+        public static final int decision=0x7f060001;
         /**  エラーメッセージ 
          */
-        public static final int err_mes_01=0x7f050003;
+        public static final int err_mes_01=0x7f060003;
         /**  ヒント 
          */
-        public static final int hint_input_name=0x7f050002;
+        public static final int hint_input_name=0x7f060002;
+        public static final int option_bgm_summary=0x7f060008;
+        public static final int option_bgm_title=0x7f060007;
+        public static final int option_button_cancel=0x7f060005;
+        public static final int option_button_ok=0x7f060004;
+        public static final int option_category_title_volume=0x7f060006;
+        public static final int option_reset_message=0x7f06000d;
+        public static final int option_reset_summary=0x7f06000c;
+        public static final int option_reset_title=0x7f06000b;
+        public static final int option_se_summary=0x7f06000a;
+        public static final int option_se_title=0x7f060009;
     }
     public static final class style {
-        public static final int defaultTheme=0x7f060000;
+        public static final int defaultTheme=0x7f070000;
+    }
+    public static final class xml {
+        public static final int option=0x7f040000;
     }
 }
--- Selector/src/com/scarviz/selector/Option.java
+++ Selector/src/com/scarviz/selector/Option.java
@@ -1,7 +1,50 @@
 package com.scarviz.selector;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
 
 public class Option extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.option);
+		setResetDialog();
+	}
+
+    /** 初期化ダイアログ設定 */
+    private void setResetDialog() {
+        PreferenceScreen pref = (PreferenceScreen)findPreference("option_reset");
+        pref.setOnPreferenceClickListener(new OnPreferenceClickListener(){
+            public boolean onPreferenceClick(Preference preference) {
+            	/** 確認ダイアログ */
+            	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(preference.getContext());
+                alertDialogBuilder.setTitle(getString(R.string.option_reset_title));
+                alertDialogBuilder.setMessage(R.string.option_reset_message);
+                /** OKボタン選択 */
+                alertDialogBuilder.setPositiveButton(R.string.option_button_ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            	 /** 初期化処理 */
+                            	 SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+                            	 editor.clear();
+                            	 editor.commit();
+                            }
+                        });
+                /** キャンセルボタン選択 */
+                alertDialogBuilder.setNegativeButton(R.string.option_button_cancel,null);
+                alertDialogBuilder.setCancelable(true);
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+				return true;
+			}});
+    }
 
 }
