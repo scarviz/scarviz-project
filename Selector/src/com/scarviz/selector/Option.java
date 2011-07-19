5a43a159a66fca77ce8c5c60cb53809a75186f62
myu_mn <myu_mn@hotmail.com>
2011-07-19 11:25:08 +0000

    追加：タイトルレイアウト、設定アクティビティ 変更：タイトルアクティビティ内容作成、AndroidManifestに設定アクティビティ追加 削除：main.xml削除

---
+++ Selector/src/com/scarviz/selector/Option.java
@@ -0,0 +1,7 @@
+package com.scarviz.selector;
+
+import android.preference.PreferenceActivity;
+
+public class Option extends PreferenceActivity {
+
+}
--- Selector/src/com/scarviz/selector/TitleActivity.java
+++ Selector/src/com/scarviz/selector/TitleActivity.java
@@ -1,13 +1,31 @@
 package com.scarviz.selector;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.title);
     }
+
+    public void onClickContinueButton(View v) {
+    	Intent intent = new Intent(this, ScenarioList.class);
+    	startActivity(intent);
+    }
+
+    public void onClickStartButton(View v) {
+    	Intent intent = new Intent(this, InputInfo.class);
+    	startActivity(intent);
+    }
+
+    public void onClickOptionButton(View v) {
+    	Intent intent = new Intent(this, Option.class);
+    	startActivity(intent);
+    }
+
 }
