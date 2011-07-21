b0b7d8b0bc4b25fb37366c6804b3082464273e7f
scarviz <scarviz@gmail.com>
2011-07-21 15:35:23 +0000

    変更：GPS機能追加 　　　ジャーナルフィールド追加 　　　各ボタンのStyleを追加

--- VoiceJournal/src/com/scarviz/voicejournal/VoiceJournalActivity.java
+++ VoiceJournal/src/com/scarviz/voicejournal/VoiceJournalActivity.java
@@ -1,13 +1,27 @@
 package com.scarviz.voicejournal;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -16,17 +30,36 @@ import android.widget.Toast;
  * @author scarviz
  *
  */
-public class VoiceJournalActivity extends Activity {
+public class VoiceJournalActivity extends Activity implements LocationListener{
 	// 返ってきた時の認証用コード(数字は適当なもので良い)
     private static final int REQUEST_CODE_VOICE_RECO = 123;
     // プロンプト表示用
     private static final String PROMPT_MES = "記録します";
+	// 改行コード
+	private static final String NEWLINE = "\n";
+
+    // LocationManager用
+    private LocationManager mManager;
+    // ジャーナル用EditText
+    EditText mtxtJournal;
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // LocationManagerのインスタンス生成
+        mManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        
+        // ジャーナル用EditText
+        mtxtJournal = (EditText)findViewById(R.id.txtJournal);
+        
+        // 退避情報が存在する場合
+        if(savedInstanceState != null){
+	        // 退避情報を再格納する
+	        GetInstanceState(savedInstanceState);
+        }
     }
     
     /**
@@ -39,6 +72,10 @@ public class VoiceJournalActivity extends Activity {
 	            // 音声認識開始
 	            StartVoiceRecognition();
 	            break;
+	    	case R.id.btnGps:
+	    		// 位置情報更新開始
+	    		StartLocationUpdates();
+	            break;
 	        default:
 	        	break;
     	}
@@ -48,7 +85,7 @@ public class VoiceJournalActivity extends Activity {
      * 音声認識を開始する。
      * 
      */
-    public void StartVoiceRecognition(){
+    private void StartVoiceRecognition(){
     	 try {
              // インテント作成
     		 // 認識された音声を文字列として取得する
@@ -92,11 +129,152 @@ public class VoiceJournalActivity extends Activity {
             
             //結果をトーストで表示する
             Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+            
+            // 結果をジャーナル用EditTextに追加
+            mtxtJournal.append(result + NEWLINE);
         }
         
         super.onActivityResult(requestCode, resultCode, data);
     }
+    
+    /**
+     * 位置情報の更新を開始する。
+     * 
+     */
+    private void StartLocationUpdates(){
+		try{
+			String provider = mManager.getBestProvider(new Criteria(), true);
+			if(provider == null){
+				// GPSが有効になっていない
+				// ダイアログを開いて有効にするように促す
+				AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+				dlg.setTitle(R.string.dlg_title);
+				dlg.setIcon(android.R.drawable.ic_dialog_alert);
+				dlg.setMessage(R.string.dlg_mes_001);
+				dlg.setPositiveButton(R.string.dlg_btn_ok, 
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								// TODO : 設定画面を開く
+							}});
+				dlg.show();
+				return;
+			}
+			// 位置情報の取得開始
+			mManager.requestLocationUpdates(provider, 0, 0, this);
 
+		}catch(Exception e){
+			// エラーログ出力
+			Log.e("err",e.toString());
+			
+			Toast.makeText(this,R.string.err_mes_002, Toast.LENGTH_SHORT).show();
+		}
+    }
+    
+	/**
+	 *  位置情報が変化したら呼び出される
+	 *  
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+		// 緯度と経度を取得する
+		double latitude = location.getLatitude();
+    	double longitude = location.getLongitude();
+    	
+    	// 表示用
+    	String locationInfo = "緯度＝"+latitude+",経度＝"+longitude;
+    	try{
+    		// 座標を住所に変換する
+    		locationInfo = ConvertAddress(latitude,longitude,this);
+    	}
+    	catch(IOException ex){
+			// エラーログ出力
+			Log.e("err",ex.toString());
+		}
+    	
+    	// 取得情報を表示
+		Toast.makeText(this, locationInfo, Toast.LENGTH_SHORT).show();
+
+        // 緯度と経度をジャーナル用EditTextに追加
+        mtxtJournal.append(locationInfo + NEWLINE);
+        
+		// リスナーを解除(ボタンワンクリックで1回取得するだけ)
+		mManager.removeUpdates(this);
+	}
+	
+	/**
+	 * 座標を住所に変換する。
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param context
+	 * @return
+	 * @throws IOException
+	 */
+	private String ConvertAddress(double latitude, double longitude, Context context) throws IOException{
+		// 戻り値用結果
+		String result = new String();
+
+		//　geocoedrの実体化
+		Geocoder geocoder = new Geocoder(context, Locale.JAPAN);
+		// 住所情報を取得(最大5項目)
+		List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 5);
+
+		// 空でない場合
+		if (!addressList.isEmpty()){
+			// 住所情報
+			Address address = addressList.get(0);
+			StringBuffer strbuf = new StringBuffer();
+
+			//　住所情報をつなげていく
+			String buf;
+			for (int i = 0; (buf = address.getAddressLine(i)) != null; i++){
+				strbuf.append(buf+"　");
+			}
+
+			result = strbuf.toString();
+		}
+		
+		return result;
+	}
+	
+    /**
+     * 退避情報を再格納する。
+     * 
+     */
+    private void GetInstanceState(Bundle inState){
+    	// 退避情報を取得する
+    	String txtJournal = inState.getString("txtJournal");	// ジャーナル内容 
+    	
+    	// 取得した値を再格納する
+    	mtxtJournal.setText(txtJournal);	// ジャーナル内容 
+    }
+    
+    /**
+     * 保持している情報を退避させる。
+     * 
+     */
+    @Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// 退避する値の取得
+    	String txtJournal = mtxtJournal.getText().toString();	// ジャーナル内容 
+    	
+    	// 情報を退避させる
+		outState.putString("txtJournal", txtJournal);	// ジャーナル内容
+	}
+
+    /**
+     * アクティビティ停止時処理
+     * 
+     */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// リスナーを解除
+		mManager.removeUpdates(this);
+	}
+	
     /**
      * アクティビティ終了時処理
      * 
@@ -104,6 +282,30 @@ public class VoiceJournalActivity extends Activity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		// グローバルな変数をGCにまわす処理を書いているが今回はなし
+		// GCに優先的に開放させる
+		mManager = null;
+		mtxtJournal = null;
+	}
+
+
+    /**
+     * プロバイダが無効になったら呼び出される
+     */
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	/**
+	 * プロバイダが有効になったら呼び出される
+	 */
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	/**
+	 * プロバイダの状態が変化したら呼び出される
+	 */
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {		
 	}
 }
