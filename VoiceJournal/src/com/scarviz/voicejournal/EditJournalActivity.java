37f9bf2d9f17d3de51cf351827b7d05c7c8db08f
scarviz <scarviz@gmail.com>
2011-07-23 21:36:15 +0000

    変更：メッセージを追加。 　　　削除時にメッセージを表示するように修正。 　　　編集内容に変化がない場合は、反映しないように修正。 　　　リスト追加時に一番上に追加するように修正。 　　　編集内容反映時に元の位置を変えないように修正。

--- VoiceJournal/src/com/scarviz/voicejournal/EditJournalActivity.java
+++ VoiceJournal/src/com/scarviz/voicejournal/EditJournalActivity.java
@@ -13,9 +13,14 @@ import android.widget.EditText;
  *
  */
 public class EditJournalActivity extends Activity {
+    // エラーコード
+    private static final int ERR_CD_POSITION = -1;
     // ジャーナル用EditText
     EditText mtxtJournal;
+    // リストの選択番号
     private int mPosition;
+    // 初期状態のジャーナル
+    private String mOriTxtJournal;
 	
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +37,57 @@ public class EditJournalActivity extends Activity {
         }
         else{
             Intent intent = getIntent();
-            mPosition = intent.getIntExtra("TITLE_NO",0);
+            mPosition = intent.getIntExtra("TITLE_NO",ERR_CD_POSITION);
             String item = intent.getStringExtra("ITEM");
-        
+            // 初期状態のジャーナルを保持
+            mOriTxtJournal = item;
     		// EditTextに表示する
     		mtxtJournal.setText(item);
         }
 	}
-	
+
+    /**
+     * キーダウンイベント。
+     * 
+     */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		//バックキーボタン押下時
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// インテントのインスタンス生成
+			Intent intent = new Intent();
+			// 編集内容を取得
+			String txtJournal = mtxtJournal.getText().toString();
+			
+			// 編集内容が初期状態と変わらない場合
+			if(txtJournal.equals(mOriTxtJournal)){
+				setResult(RESULT_CANCELED, intent);
+				finish();
+				return false;
+			}
+			
+			// 呼び出し元に返す
+			intent.putExtra("TITLE_NO", mPosition);
+			intent.putExtra("ITEM", txtJournal);
+			setResult(RESULT_OK, intent);
+			finish();
+			return false;
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
+
     /**
      * 退避情報を再格納する。
      * 
      */
     private void GetInstanceState(Bundle inState){
     	// 退避情報を取得する
-    	int position = inState.getInt("position");	// リストの選択番号
+    	mPosition = inState.getInt("position");					// リストの選択番号
     	String txtJournal = inState.getString("txtJournal");	// ジャーナル内容 
-    	
+    	mOriTxtJournal = inState.getString("oriTxtJournal");	// 初期状態のジャーナル 
+
     	// 取得した値を再格納する
-    	mPosition = position;	// リストの選択番号
     	mtxtJournal.setText(txtJournal);	// ジャーナル内容 
     }
     
@@ -65,33 +102,21 @@ public class EditJournalActivity extends Activity {
     	String txtJournal = mtxtJournal.getText().toString();	// ジャーナル内容 
     	
     	// 情報を退避させる
-		outState.putInt("position", mPosition);	// リストの選択番号
-		outState.putString("txtJournal", txtJournal);	// ジャーナル内容
-	}
-    
-    
-    
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		//バックキーボタン押下時
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-				// 呼び出し元に返す
-				Intent intent = new Intent();
-				intent.putExtra("TITLE_NO", mPosition);
-				intent.putExtra("ITEM", mtxtJournal.getText().toString());
-				setResult(RESULT_OK, intent);
-				finish();
-				return false;
-			}
-
-		return super.onKeyDown(keyCode, event);
+		outState.putInt("position", mPosition);					// リストの選択番号
+		outState.putString("txtJournal", txtJournal);			// ジャーナル内容
+		outState.putString("oriTxtJournal", mOriTxtJournal);	// 初期状態のジャーナル
 	}
 
+    /**
+     * アクティビティ終了時処理。
+     * 
+     */
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		// GCに優先的に開放させる
 		mtxtJournal = null;
+		mOriTxtJournal = null;
 	}
 
 }
