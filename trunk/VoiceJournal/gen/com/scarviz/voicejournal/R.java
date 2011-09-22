a5b3a5335a8a794352f23cc9cac9afcbad484e26
scarviz@gmail.com <scarviz@gmail.com@36ffe54a-474d-a20e-a963-f37414e2a6ad>
2011-09-22 10:56:43 +0000

    追加：設定画面 　　　リストライン 　　　設定画面リスト 変更：メニューに設定画面を呼び出せるように追加 　　　設定画面に対応するように修正

--- trunk/VoiceJournal/gen/com/scarviz/voicejournal/R.java
+++ trunk/VoiceJournal/gen/com/scarviz/voicejournal/R.java
@@ -8,42 +8,60 @@
 package com.scarviz.voicejournal;
 
 public final class R {
+    public static final class array {
+        public static final int line_list=0x7f050000;
+        public static final int line_list_values=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040000;
-        public static final int green=0x7f040005;
-        public static final int orange=0x7f040004;
-        public static final int pink=0x7f040002;
-        public static final int red=0x7f040003;
-        public static final int white=0x7f040001;
+        public static final int black=0x7f060000;
+        public static final int green=0x7f060005;
+        public static final int orange=0x7f060004;
+        public static final int pink=0x7f060002;
+        public static final int red=0x7f060003;
+        public static final int white=0x7f060001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int mylocation=0x7f020001;
-        public static final int mylocation2=0x7f020002;
-        public static final int mylocation_state=0x7f020003;
-        public static final int search=0x7f020004;
-        public static final int search2=0x7f020005;
-        public static final int search_state=0x7f020006;
-        public static final int voice=0x7f020007;
-        public static final int voice2=0x7f020008;
-        public static final int voice_state=0x7f020009;
+        public static final int btn_media_player_disabled=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int line_black=0x7f020002;
+        public static final int line_blue=0x7f020003;
+        public static final int line_brown=0x7f020004;
+        public static final int line_gray=0x7f020005;
+        public static final int line_green=0x7f020006;
+        public static final int line_lightblue=0x7f020007;
+        public static final int line_orange=0x7f020008;
+        public static final int line_pink=0x7f020009;
+        public static final int line_purple=0x7f02000a;
+        public static final int line_red=0x7f02000b;
+        public static final int line_white=0x7f02000c;
+        public static final int line_yellow=0x7f02000d;
+        public static final int mylocation=0x7f02000e;
+        public static final int mylocation2=0x7f02000f;
+        public static final int mylocation_state=0x7f020010;
+        public static final int search=0x7f020011;
+        public static final int search2=0x7f020012;
+        public static final int search_state=0x7f020013;
+        public static final int voice=0x7f020014;
+        public static final int voice2=0x7f020015;
+        public static final int voice_state=0x7f020016;
     }
     public static final class id {
-        public static final int btnGps=0x7f080006;
-        public static final int btnRec=0x7f080005;
-        public static final int btnSearch=0x7f080007;
-        public static final int edit=0x7f080001;
-        public static final int header_menu=0x7f080004;
-        public static final int journal=0x7f080003;
-        public static final int menu_add=0x7f08000b;
-        public static final int menu_background=0x7f08000c;
-        public static final int menu_cancel=0x7f08000a;
-        public static final int search_layout=0x7f080008;
-        public static final int txtDate=0x7f080000;
-        public static final int txtJournal=0x7f080002;
-        public static final int txtSearch=0x7f080009;
+        public static final int btnBack=0x7f0a0001;
+        public static final int btnGps=0x7f0a0007;
+        public static final int btnRec=0x7f0a0006;
+        public static final int btnSearch=0x7f0a0008;
+        public static final int edit=0x7f0a0002;
+        public static final int header_menu=0x7f0a0005;
+        public static final int journal=0x7f0a0004;
+        public static final int menu_add=0x7f0a000c;
+        public static final int menu_cancel=0x7f0a000b;
+        public static final int menu_pref=0x7f0a000d;
+        public static final int search_layout=0x7f0a0009;
+        public static final int txtDate=0x7f0a0000;
+        public static final int txtJournal=0x7f0a0003;
+        public static final int txtSearch=0x7f0a000a;
     }
     public static final class layout {
         public static final int customtitle=0x7f030000;
@@ -51,37 +69,90 @@ public final class R {
         public static final int journallist=0x7f030002;
     }
     public static final class menu {
-        public static final int editmenu=0x7f070000;
-        public static final int mainmenu=0x7f070001;
+        public static final int editmenu=0x7f090000;
+        public static final int mainmenu=0x7f090001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int dlg_btn_cancel=0x7f050006;
+        public static final int app_name=0x7f070000;
+        public static final int btn_back=0x7f070005;
+        public static final int dlg_btn_cancel=0x7f07000a;
         /**  ダイアログ用 
          */
-        public static final int dlg_btn_ok=0x7f050005;
-        public static final int dlg_del_mes_001=0x7f05000b;
-        public static final int dlg_del_title=0x7f05000a;
-        public static final int dlg_mes_001=0x7f050008;
-        public static final int dlg_selected_title=0x7f050009;
-        public static final int dlg_title=0x7f050007;
+        public static final int dlg_btn_ok=0x7f070009;
+        public static final int dlg_del_mes_001=0x7f07000f;
+        public static final int dlg_del_title=0x7f07000e;
+        public static final int dlg_mes_001=0x7f07000c;
+        public static final int dlg_selected_title=0x7f07000d;
+        public static final int dlg_title=0x7f07000b;
         /**  エラーメッセージ 
          */
-        public static final int err_mes_001=0x7f05000f;
-        public static final int err_mes_002=0x7f050010;
-        public static final int err_mes_003=0x7f050011;
-        public static final int err_mes_004=0x7f050012;
-        public static final int hint_search=0x7f050003;
+        public static final int err_mes_001=0x7f07001d;
+        public static final int err_mes_002=0x7f07001e;
+        public static final int err_mes_003=0x7f07001f;
+        public static final int err_mes_004=0x7f070020;
+        public static final int hint_search=0x7f070003;
+        public static final int line_black=0x7f070022;
+        public static final int line_blue=0x7f070023;
+        public static final int line_brown=0x7f070024;
+        /**  ラインID 
+         */
+        public static final int line_default=0x7f070021;
+        public static final int line_gray=0x7f070025;
+        public static final int line_green=0x7f070026;
+        public static final int line_lightblue=0x7f070027;
+        public static final int line_orange=0x7f070028;
+        public static final int line_pink=0x7f070029;
+        public static final int line_purple=0x7f07002a;
+        public static final int line_red=0x7f07002b;
+        public static final int line_white=0x7f07002c;
+        public static final int line_yellow=0x7f07002d;
         /**  メニュー 
          */
-        public static final int menu_add_title=0x7f05000c;
-        public static final int menu_background_title=0x7f05000d;
-        public static final int menu_cancel_title=0x7f05000e;
-        public static final int mes_edit_del=0x7f050002;
-        public static final int mes_edit_save=0x7f050001;
-        public static final int mes_share=0x7f050004;
+        public static final int menu_add_title=0x7f070010;
+        public static final int menu_cancel_title=0x7f070012;
+        public static final int menu_pref_title=0x7f070011;
+        public static final int mes_edit_del=0x7f070002;
+        public static final int mes_edit_save=0x7f070001;
+        public static final int mes_pref_bg_reset=0x7f070008;
+        public static final int mes_pref_bg_set=0x7f070007;
+        public static final int mes_share=0x7f070004;
+        public static final int pref_background_reset=0x7f070018;
+        public static final int pref_background_reset_summary=0x7f070019;
+        public static final int pref_background_reset_title=0x7f070017;
+        public static final int pref_background_select=0x7f070015;
+        public static final int pref_background_select_summary=0x7f070016;
+        public static final int pref_background_select_title=0x7f070014;
+        /**  設定画面 
+         */
+        public static final int pref_background_title=0x7f070013;
+        public static final int pref_line_select=0x7f07001c;
+        public static final int pref_line_select_title=0x7f07001b;
+        public static final int pref_line_title=0x7f07001a;
+        public static final int pref_title=0x7f070006;
+        public static final int value_black=0x7f07002f;
+        public static final int value_blue=0x7f070030;
+        public static final int value_brown=0x7f070031;
+        /**  ラインValue 
+         */
+        public static final int value_default=0x7f07002e;
+        public static final int value_gray=0x7f070032;
+        public static final int value_green=0x7f070033;
+        public static final int value_lightblue=0x7f070034;
+        public static final int value_orange=0x7f070035;
+        public static final int value_pink=0x7f070036;
+        public static final int value_purple=0x7f070037;
+        public static final int value_red=0x7f070038;
+        public static final int value_white=0x7f070039;
+        public static final int value_yellow=0x7f07003a;
     }
     public static final class style {
-        public static final int defaultTheme=0x7f060000;
+        public static final int CustomTheme_Black=0x7f080001;
+        public static final int CustomTheme_main=0x7f080002;
+        public static final int TextAppearance_WindowTitle=0x7f080004;
+        public static final int WindowTitleBackground=0x7f080003;
+        public static final int defaultTheme=0x7f080000;
+    }
+    public static final class xml {
+        public static final int pref=0x7f040000;
     }
 }
